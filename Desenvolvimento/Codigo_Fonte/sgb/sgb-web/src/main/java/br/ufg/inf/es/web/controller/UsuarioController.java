66908ad2710aa0379563b6733866a30d07040abd
0wilker0 <0wilker0@gmail.com>
2012-11-30 22:24:12 +0000

    #20 - Cração do Listar Usuário

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/UsuarioController.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/UsuarioController.java
@@ -1,12 +1,12 @@
 package br.ufg.inf.es.web.controller;
 
-import br.ufg.inf.es.base.validation.ValidationException;
 import br.ufg.inf.es.integracao.PerfilService;
 import br.ufg.inf.es.integracao.UsuarioService;
 import br.ufg.inf.es.model.Perfil;
 import br.ufg.inf.es.model.Usuario;
 import br.ufg.inf.es.web.controller.form.UsuarioForm;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -22,12 +22,11 @@ public class UsuarioController
 
     @Autowired
     private UsuarioForm form;
-    
     @Autowired
     private UsuarioService service;
-    
     @Autowired
     private PerfilService perfilService;
+    private Collection<Usuario> usuarioSelecionado;
 
     @Override
     public UsuarioForm getForm() {
@@ -52,16 +51,33 @@ public class UsuarioController
     }
 
     public PerfilService getPerfilService() {
-        
+
         return perfilService;
     }
 
     public void setPerfilService(PerfilService perfilService) {
-        
+
         this.perfilService = perfilService;
     }
-    
-    public List<Perfil> getPerfil(){
+
+    public List<Perfil> getPerfil() {
         return new ArrayList<Perfil>(this.perfilService.getDAO().list());
+    }
+
+    public Collection<Usuario> getUsuarioSelecionado() {
+        return usuarioSelecionado;
+    }
+
+    public void setUsuarioSelecionado(Collection<Usuario> usuarioSelecionado) {
+        this.usuarioSelecionado = usuarioSelecionado;
+    }
+
+
+    public void selecionaUsuario(Usuario usuario) {
+        this.usuarioSelecionado.add(usuario);
+    }
+
+    public void removerUsuarioSelecionados() {
+            this.service.getDAO().removeAll(usuarioSelecionado);
     }
 }
