ddc5d96953ad22d9a2ef2d2dabef20671a053b9b
rodrigo@engenhariadesoftware.inf.br <rodrigo@engenhariadesoftware.inf.br@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2012-11-30 22:02:39 +0000

    #120 cadastro, busca e exclusão de disciplinas

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/DisciplinaService.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/DisciplinaService.java
@@ -5,6 +5,8 @@
 package br.ufg.inf.es.integracao;
 
 import br.ufg.inf.es.base.persistence.DAO;
+import br.ufg.inf.es.base.validation.ValidationException;
+import br.ufg.inf.es.integracao.annotations.RNG006;
 import br.ufg.inf.es.model.Disciplina;
 import br.ufg.inf.es.persistencia.DisciplinaDAO;
 import br.ufg.inf.es.persistencia.LivroDAO;
@@ -33,4 +35,13 @@ public class DisciplinaService extends GenericService<Disciplina> {
 
         this.dao = dao;
     }
+
+    @Override
+    @RNG006
+    public Long insert(Disciplina entidade) throws ValidationException {
+        
+        return this.getDAO().insert(entidade);
+    }
+    
+    
 }
--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/LivroController.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/LivroController.java
@@ -6,8 +6,11 @@ package br.ufg.inf.es.web.controller;
 
 import br.ufg.inf.es.integracao.DisciplinaService;
 import br.ufg.inf.es.integracao.LivroService;
+import br.ufg.inf.es.model.Autor;
 import br.ufg.inf.es.model.Livro;
 import br.ufg.inf.es.web.controller.form.LivroForm;
+import java.util.ArrayList;
+import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -45,6 +48,17 @@ public class LivroController extends SGBController<Livro, LivroForm, LivroServic
         this.service = service;
     }
     
+    public Collection<Livro> complete(String query) {  
+        Collection<Livro> results = new ArrayList<Livro>();
+          
+        for(Livro autor : form.getCollectionEntities()){
+            if(autor.getTitulo().contains(query)){
+                results.add(autor);
+            }
+        }
+          
+        return results;  
+    }
     
     
 }
