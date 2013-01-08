73f58c8675c4afdf89ec670e71cb429143d21e88
0wilker0@gmail.com <0wilker0@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-01-08 16:52:23 +0000

    ref #337 - Continuando a implementação do caso de uso US13

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/EditoraController.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/EditoraController.java
@@ -1,8 +1,11 @@
 package br.ufg.inf.es.web.controller;
 
+import br.ufg.inf.es.base.validation.ValidationException;
 import br.ufg.inf.es.integracao.EditoraService;
 import br.ufg.inf.es.model.Editora;
 import br.ufg.inf.es.web.controller.form.EditoraForm;
+import java.util.Collection;
+import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -15,12 +18,13 @@ import org.springframework.stereotype.Component;
 public class EditoraController
         extends SGBController<Editora, EditoraForm, EditoraService> {
 
+    private String buscaEditora = "";
+    private Collection<Editora> editoraSelecionado = new HashSet<Editora>();
     @Autowired
     private EditoraForm form;
-    
     @Autowired
     private EditoraService service;
-    
+
     @Override
     public EditoraForm getForm() {
 
@@ -34,14 +38,48 @@ public class EditoraController
     }
 
     public void setForm(EditoraForm form) {
-        
+
         this.form = form;
     }
 
     public void setService(EditoraService service) {
-        
+
         this.service = service;
     }
+
+    public String salvarEditora() throws ValidationException {
+
+        super.insert();
+
+        return super.openSearchPage();
+
+    }
+
+    public void selecionaEditora(Editora assTab) {
+        
+        this.editoraSelecionado.add(assTab);
+        
+    }
+
+    public void removerEditoraSelecionadas() {
+        
+        this.service.getDAO().removeAll(editoraSelecionado);  
+        
+    }
     
-    
+    public String getBuscaEditora() {
+        return buscaEditora;
+    }
+
+    public void setBuscaEditora(String buscaEditora) {
+        this.buscaEditora = buscaEditora;
+    }
+
+    public Collection<Editora> getEditoraSelecionado() {
+        return editoraSelecionado;
+    }
+
+    public void setEditoraSelecionado(Collection<Editora> editoraSelecionado) {
+        this.editoraSelecionado = editoraSelecionado;
+    }
 }
