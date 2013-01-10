c9d09935c0ffa93a309632a8c5b1b2a9ed2ca03b
0wilker0@gmail.com <0wilker0@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-01-10 16:15:18 +0000

    ref #337 - Implementação do caso de uso US13 concluida {atualizei a versão do primefaces para 3.4.2 no pom.xml}

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/EditoraController.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/EditoraController.java
@@ -20,6 +20,7 @@ public class EditoraController
 
     private String buscaEditora = "";
     private Collection<Editora> editoraSelecionado = new HashSet<Editora>();
+    private Editora editora = new Editora();
     @Autowired
     private EditoraForm form;
     @Autowired
@@ -55,31 +56,87 @@ public class EditoraController
 
     }
 
-    public void selecionaEditora(Editora assTab) {
-        
-        this.editoraSelecionado.add(assTab);
-        
+    public String editarEditora() throws ValidationException {
+
+        super.edit();
+
+        return super.openSearchPage();
+
     }
 
-    public void removerEditoraSelecionadas() {
-        
-        this.service.getDAO().removeAll(editoraSelecionado);  
-        
+    public void selecionaEditora(Editora assTab) {
+
+        if (this.editoraSelecionado.contains(assTab)) {
+
+            this.editoraSelecionado.remove(assTab);
+
+        } else {
+
+            this.editoraSelecionado.add(assTab);
+
+        }
+
     }
-    
+
+    public void preparaEditora(Editora assTab) {
+
+        this.editora = assTab;
+
+    }
+
+    public String removerEditora() {
+
+        this.service.getDAO().remove(this.editora);
+
+        return super.openSearchPage();
+
+    }
+
+    public String removerEditoraSelecionadas() {
+
+        this.service.getDAO().removeAll(editoraSelecionado);
+
+        editoraSelecionado = new HashSet<Editora>();
+
+        this.form.setCollectionEntities(this.service.getDAO().list());
+
+        return super.openSearchPage();
+
+    }
+
     public String getBuscaEditora() {
+
         return buscaEditora;
+
     }
 
     public void setBuscaEditora(String buscaEditora) {
+
         this.buscaEditora = buscaEditora;
+
     }
 
     public Collection<Editora> getEditoraSelecionado() {
+
         return editoraSelecionado;
+
     }
 
     public void setEditoraSelecionado(Collection<Editora> editoraSelecionado) {
+
         this.editoraSelecionado = editoraSelecionado;
+
+    }
+
+    public Editora getEditora() {
+
+        return editora;
+
+    }
+
+    public void setEditora(Editora editora) {
+
+        this.editora = editora;
+
     }
 }
