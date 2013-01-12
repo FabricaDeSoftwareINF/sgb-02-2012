e71ab80f8249f3c9974bee6b9347f1dc7288fb2f
vinicius.esufg@gmail.com <vinicius.esufg@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-01-12 15:51:51 +0000

    ref #468 - Criação de uma implementação parcial da integração com um sistema para importar dados; criação de arquivos json com o formato para a comunicação dos dados e de um servidor mock em ruby para retornar estes dados;

---
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/main/java/br/ufg/inf/es/model/importacaobibliografia/DisciplinaImportada.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.es.model.importacaobibliografia;
+
+import java.util.List;
+
+/**
+ *
+ * @author Vinícius
+ */
+public class DisciplinaImportada {
+    
+    private String nome;
+    private String codigo;
+    private BibliografiaImportada bibliografia;
+
+    public DisciplinaImportada() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public BibliografiaImportada getBibliografia() {
+        return bibliografia;
+    }
+
+    public void setBibliografia(BibliografiaImportada bibliografia) {
+        this.bibliografia = bibliografia;
+    }
+
+    @Override
+    public String toString() {
+        return "DisciplinaImportada{" + "nome=" + nome + ", codigo=" 
+                + codigo + ", bibliografia=" + bibliografia + '}';
+    }
+
+}
