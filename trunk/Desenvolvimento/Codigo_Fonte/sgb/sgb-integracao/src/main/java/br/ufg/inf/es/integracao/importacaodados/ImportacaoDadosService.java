2f3e0526c6bbab4920c6839eb88c9f7bce53a727
kssiofreitas@gmail.com <kssiofreitas@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-01-17 23:56:35 +0000

    ref #336 Ajustes no US3.

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/importacaodados/ImportacaoDadosService.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/importacaodados/ImportacaoDadosService.java
@@ -4,23 +4,47 @@ import br.ufg.inf.es.model.Curso;
 import br.ufg.inf.es.model.Disciplina;
 
 /**
- *
- * @author Vinícius
+ * Interface de comunicação com Serviços de Importação
+ * @author Vinícius Gonçalves
  */
 public interface ImportacaoDadosService {
     
+    /**
+     * 
+     * @param curso 
+     */
     public void importarBibliografia(Curso curso);
     
+    /**
+     * 
+     * @param nome 
+     */
     public void importarCurso(String nome);
     
+    /**
+     * 
+     */
     public void importarCursos();
     
+    /**
+     * 
+     * @param Disciplina 
+     */
     public void importarLivros(Disciplina Disciplina) ;
     
+    /**
+     * 
+     */
     public void importarLivros();
     
+    /**
+     * 
+     */
     public void importarEditoras();
     
+    /**
+     * 
+     */
     public void importarAutores();
     
 }
--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/JSFController.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/JSFController.java
@@ -29,7 +29,7 @@ public abstract class JSFController extends BaseController implements Controller
 
     private String getOpenInitialPageNavigator() {
         
-        return this.getRootNavigation() + "OP";
+        return this.getRootNavigation() + "initialPage";
     }
 
     protected String getRootNavigation() {
