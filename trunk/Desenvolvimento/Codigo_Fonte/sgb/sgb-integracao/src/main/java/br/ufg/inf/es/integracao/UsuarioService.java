a82cf9da349b1632d2a5d21ec774e69a205a8bd9
kssiofreitas@gmail.com <kssiofreitas@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-01-18 23:52:54 +0000

    ref #578 realização de comentários na US3.

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/UsuarioService.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/UsuarioService.java
@@ -21,61 +21,61 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 /**
+ * Classe de serviços relativos a US03 de Manter Usuário
+ *
  * @author Cézar Augusto Ferreira
  */
 @Component
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UsuarioService extends GenericService<Usuario> {
 
+    /**
+     * Atributo UsuarioDAO
+     */
     @Autowired
     private UsuarioDAO dao;
-    
+    /**
+     * Atributo UsuarioPerfilDAO
+     */
     @Autowired
     private UsuarioPerfilDAO usuarioPerfilDao;
-    
+    /**
+     * Atributo PerfilDAO
+     */
     @Autowired
     private PerfilDAO perfilDao;
-    
-    private static final SecureRandom GERADOR_RANDOM = new SecureRandom();
-    
     /**
-     * Tamanho da senha
+     * Constante GERADOR_RANDOM
+     */
+    private static final SecureRandom GERADOR_RANDOM = new SecureRandom();
+    /**
+     * Constante PASSWORD_LENCHT
      */
     private static final Integer PASSWORD_LENGHT = 8;
-    
+    /**
+     * Atributo cryptography
+     */
     private SgbCryptography cryptography = new SgbCryptography();
-
-    @Override
-    public UsuarioDAO getDAO() {
-
-        return this.dao;
-    }
-
-    public void setDao(UsuarioDAO dao) {
-
-        this.dao = dao;
-    }
-
-    public UsuarioPerfilDAO getUsuarioPerfilDao() {
-        return usuarioPerfilDao;
-    }
-
-    public void setUsuarioPerfilDao(UsuarioPerfilDAO usuarioPerfilDao) {
-        this.usuarioPerfilDao = usuarioPerfilDao;
-    }
 
     @Override
     @RNG001
     @RNG002
     @RNG003
+    /**
+     * Método responsável por inserir uma entidade Usuário
+     *
+     * @param entity
+     * @return id da Entidade persistida.
+     * @throws ValidationException
+     */
     public Long insert(Usuario entity) throws ValidationException {
 
         entity.setDataCadastro(new Date());
-        
+
         String oldPass = entity.getSenha();
-        
+
         String passEncrypted = cryptography.encrypt(oldPass);
-        
+
         entity.setSenha(passEncrypted);
 
         entity.setStatus(true);
@@ -83,11 +83,11 @@ public class UsuarioService extends GenericService<Usuario> {
         Long id = super.insert(entity);
 
         for (Perfil perfil : entity.getPerfil()) {
-            
+
             UsuarioPerfil usuarioPerfil = new UsuarioPerfil();
-            
+
             usuarioPerfil.setIdUsuario(id);
-            
+
             usuarioPerfil.setIdPerfil(perfil.getId());
 
             usuarioPerfilDao.insert(usuarioPerfil);
@@ -100,30 +100,41 @@ public class UsuarioService extends GenericService<Usuario> {
     @RNG001
     @RNG002
     @RNG003
+    /**
+     * Método responsável por atualizar uma entidade;
+     *
+     * @param entity
+     * @throws ValidationException
+     */
     public void update(Usuario entity) throws ValidationException {
 
         usuarioPerfilDao.removeAll(usuarioPerfilDao.list(entity.getId()));
 
         entity.setStatus(true);
-        
+
         String passEncrypted = cryptography.encrypt(entity.getSenha());
-        
+
         entity.setSenha(passEncrypted);
 
         super.update(entity);
 
         for (Perfil perfil : entity.getPerfil()) {
-            
+
             UsuarioPerfil usuarioPerfil = new UsuarioPerfil();
-            
+
             usuarioPerfil.setIdPerfil(perfil.getId());
-            
+
             usuarioPerfil.setIdUsuario(entity.getId());
 
             usuarioPerfilDao.insert(usuarioPerfil);
         }
     }
 
+    /**
+     * Método responsável por remover uma entidade.
+     * @param entity
+     * @throws ValidationException 
+     */
     @Override
     public void remove(Usuario entity) throws ValidationException {
 
@@ -132,11 +143,16 @@ public class UsuarioService extends GenericService<Usuario> {
         super.remove(entity);
     }
 
+    /**
+     * Método responsável por remover uma coleção de entidades.
+     * @param collectionEntities
+     * @throws ValidationException 
+     */
     @Override
     public void removeAll(Collection<Usuario> collectionEntities) throws ValidationException {
 
         for (Usuario usuario : collectionEntities) {
-           
+
             Collection<UsuarioPerfil> colUsuarioPerfil = usuarioPerfilDao.list(usuario.getId());
 
             usuarioPerfilDao.removeAll(colUsuarioPerfil);
@@ -144,6 +160,10 @@ public class UsuarioService extends GenericService<Usuario> {
 
         super.removeAll(collectionEntities);
     }
+    
+    /**
+     * Método responsável por pesquisar uma entidade Usuário.
+     */
 
     @Override
     public Collection<Usuario> search(Usuario entity) {
@@ -164,6 +184,11 @@ public class UsuarioService extends GenericService<Usuario> {
 
         return colUsuario;
     }
+    
+    /**
+     * Método responsável por listar numa coleção todas as entidades do tipo usuário
+     * @return coleção de usuário
+     */
 
     @Override
     public Collection<Usuario> list() {
@@ -173,13 +198,13 @@ public class UsuarioService extends GenericService<Usuario> {
         for (Usuario usuario : colUsuario) {
 
             Collection<UsuarioPerfil> colUsuarioPerfil = usuarioPerfilDao.list(usuario.getId());
-            
+
             HashSet<Perfil> hsPerfil = new HashSet<Perfil>();
 
             for (UsuarioPerfil usuPerfil : colUsuarioPerfil) {
-                
+
                 Perfil perfil = perfilDao.find(usuPerfil.getIdPerfil());
-                
+
                 hsPerfil.add(perfil);
             }
             usuario.setPerfil(hsPerfil);
@@ -187,67 +212,151 @@ public class UsuarioService extends GenericService<Usuario> {
 
         return colUsuario;
     }
+    
+    /**
+     * Método responsável por sincronizar os dados da entidade Usuário com 
+     * demais dependencias de entidades 
+     * 
+     * @param entity 
+     */
 
     @Override
     public void refresh(Usuario entity) {
 
         UsuarioPerfil usuarioPerfil = new UsuarioPerfil();
-        
+
         usuarioPerfil.setIdUsuario(entity.getId());
-        
+
         Collection<UsuarioPerfil> colUsuarioPerfil = usuarioPerfilDao.search(usuarioPerfil);
-        
+
         HashSet<Perfil> hsPerfil = new HashSet<Perfil>();
-        
+
         for (UsuarioPerfil usuPerfil : colUsuarioPerfil) {
-            
+
             Perfil perfil = perfilDao.find(usuPerfil.getIdPerfil());
-            
+
             hsPerfil.add(perfil);
         }
-        
+
         entity.setPerfil(hsPerfil);
 
     }
 
+    /**
+     * Método responsável por carregar os perfis de um determinado usuário
+     * @param usuario
+     * @return <code>Coleção de Perfil</code>
+     */
     public Collection<Perfil> carreguePerfis(Usuario usuario) {
-        
+
         return usuarioPerfilDao.listPerfis(usuario.getId());
     }
+    
+    /**
+     * Método responsável por autenticar os dados informados para o Usuário
+     * @param user
+     * @param password
+     * @return usuário
+     */
 
     public Usuario authUser(String user, String password) {
-        
+
         return dao.findUserByEmailAndPassword(user, password);
     }
 
+    /**
+     * Método responsável por recuperar a senha do usuário de acordo com seu email cadastrado
+     * @param emailUsuario 
+     */
     public void recuperarSenha(String emailUsuario) {
-        
+
         Usuario usuario = dao.findUserByEmail(emailUsuario);
-        
-        if(usuario == null) {
-            
+
+        if (usuario == null) {
+
             return;
         }
-        
+
         String letters = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789_-";
 
         String newPass;
-        
+
         StringBuilder newPasswordBuilder = new StringBuilder();
-                
-        for (int i=0; i< UsuarioService.PASSWORD_LENGHT; i++){
-            
-            int index = (int)(UsuarioService.GERADOR_RANDOM.nextDouble() * letters.length());
-            
-            newPasswordBuilder.append(letters.substring(index, index+1));
+
+        for (int i = 0; i < UsuarioService.PASSWORD_LENGHT; i++) {
+
+            int index = (int) (UsuarioService.GERADOR_RANDOM.nextDouble() * letters.length());
+
+            newPasswordBuilder.append(letters.substring(index, index + 1));
         }
-        
+
         newPass = newPasswordBuilder.toString();
-        
+
         usuario.setSenha(newPass);
+       
         EmailService.enviarNovaSenha(usuario);
+        
         usuario.setSenha(cryptography.encrypt(newPass));
+        
         dao.update(usuario);
+        
         dao.update(usuario);
+    }
+
+    /**
+     * Método Responsável por obter o usuário DAO
+     *
+     * @return
+     * <code>UsuarioDAO</code>
+     */
+    @Override
+    public UsuarioDAO getDAO() {
+
+        return this.dao;
+    }
+
+    /**
+     * Método para setar uma Classe dao ao service
+     *
+     * @param dao
+     */
+    public void setDao(UsuarioDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * Método responsável por setar um novo valor para entidade de Criptografia
+     *
+     * @param cryptography
+     */
+    public void setCryptography(SgbCryptography cryptography) {
+        this.cryptography = cryptography;
+    }
+
+    /**
+     * Método responsável por setar um dao para entidade Perfil
+     *
+     * @param perfilDao
+     */
+    public void setPerfilDao(PerfilDAO perfilDao) {
+        this.perfilDao = perfilDao;
+    }
+
+    /**
+     * Método responsável por obter o dao da entidade UsuarioPerfil
+     */
+    public UsuarioPerfilDAO getUsuarioPerfilDao() {
+
+        return this.usuarioPerfilDao;
+    }
+
+    /**
+     * Método responsável por setar um dao da classe
+     * <code>UsuárioPerfilDao</code>
+     *
+     * @param <code>usuarioPerfilDao</code>
+     */
+    public void setUsuarioPerfilDao(UsuarioPerfilDAO usuarioPerfilDao) {
+        this.usuarioPerfilDao = usuarioPerfilDao;
     }
 }
--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/UsuarioController.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/UsuarioController.java
@@ -14,88 +14,167 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- * @author Cézar Augusto Ferreira
+ * Classe responsável por realizar o controle de acões e dados relativos
+ * ao usuário.
+ * 
+ * @author Cézar Augusto Ferreira, Cássio Augusto Silva de Freitas, Luã Silvério,
+ * Diogo Ribeiro
  */
 @Component
 @Scope("session")
 public class UsuarioController
         extends SGBController<Usuario, UsuarioForm, UsuarioService> {
 
+    /**
+     * Atributo form
+     */
     @Autowired
     private UsuarioForm form;
+    
+    /**
+     * Atributo service
+     */
     @Autowired
     private UsuarioService service;
+    
+    /**
+     * Atributo perfilService
+     */
     @Autowired
     private PerfilService perfilService;
+    
+    /**
+     * Atributo coleção de usuários selecionados
+     */
     private Collection<Usuario> usuarioSelecionado = new HashSet<Usuario>();
-
+   
+    /**
+     * Método responsável por obter o formulário de usuário
+     * 
+     * @return <code>UsuarioForm</code>
+     */
     @Override
     public UsuarioForm getForm() {
 
         return this.form;
     }
 
+    /**
+     * Método responsável por obter o Service de Usuário
+     * 
+     * @return <code>UsuarioService</code> 
+     */
     @Override
     public UsuarioService getService() {
 
         return this.service;
     }
 
-    public void setForm(UsuarioForm form) {
-
-        this.form = form;
-    }
-
-    public void setService(UsuarioService service) {
-
-        this.service = service;
-    }
-
-    public PerfilService getPerfilService() {
-
-        return perfilService;
-    }
-
-    public void setPerfilService(PerfilService perfilService) {
-
-        this.perfilService = perfilService;
-    }
-
+    /**
+     * Método responsável por buscar os perfis de acesso ao sistema
+     * 
+     * @return  <code>Lista de Perfil</code>
+     */
     public List<Perfil> getPerfil() {
+            
         return new ArrayList<Perfil>(this.perfilService.getDAO().list());
+  
     }
 
+    /**
+     * Método responsável obter a coleção de usuários selecionados.
+     * 
+     * @return 
+     */
     public Collection<Usuario> getUsuarioSelecionado() {
+     
         return usuarioSelecionado;
+  
     }
 
+    /**
+     * Método responsável por setar uma nova coleção de usuários selecionados.
+     * 
+     * @param usuarioSelecionado 
+     */
     public void setUsuarioSelecionado(Collection<Usuario> usuarioSelecionado) {
+   
         this.usuarioSelecionado = usuarioSelecionado;
+    
     }
 
+    /**
+     * Método responsável por adicionar um usuário na Coleção de Usuários Selecionados.
+     * 
+     * @param usuario 
+     */
     public void selecionaUsuario(Usuario usuario) {
+   
         this.usuarioSelecionado.add(usuario);
+   
     }
 
+    /**
+     * Método responsável por remover os usuários selecionados.
+     */
     public void removerUsuarioSelecionados() {
         
         this.service.getDAO().removeAll(usuarioSelecionado);  
-    }
-    
-    public String editarUsuario() {
-        super.edit();
-        return super.openSearchPage();
-    }
-    
-    public String criarUsuario() {
-        super.insert();
-        return super.openSearchPage();
-    }
-    
-    public String janelaRecuperarSenha(){
-        return this.getRootNavigation() + "recuperarSenha";
+   
     }
 
+    /**
+     * Método responsável iniciar os dados do form.
+     */
+    @Override
+    public void initData() {
+        
+        this.getForm().setTabelaUsuarios(new ArrayList<Usuario>());
+        
+        this.getForm().getTabelaUsuarios().addAll(this.getService().list());
+    }
+    
+    /**
+     * Método responsável por editar um usuário
+     * 
+     * @return string de navegação
+     */
+    public String editarUsuario() {
+        
+        super.edit();
+        
+        return super.openInitialPage();
+    }
+    
+    /**
+     * Método responsável por criar um novo usuário
+     * 
+     * @return String de navegação para página inicial.
+     */
+    public String criarUsuario() {
+        
+        super.insert();    
+        
+        return super.openInitialPage();
+    }
+    
+    /**
+     * Método responsável por retornar string de para pagina de recuperação de 
+     * senha
+     * 
+     * @return string de navegação para a página de recuperação de senha. 
+     */
+    public String janelaRecuperarSenha(){
+       
+        return this.getRootNavigation() + "recuperarSenha";
+    
+    }
+
+    /**
+     * Método responsável recuperar a senha
+     * 
+     * @return String de navegação para página de login.
+     */
     public String recuperarSenha(){
         String email = this.getParameterFromRequest("email");
         service.recuperarSenha(email);
