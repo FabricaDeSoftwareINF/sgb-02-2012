7ec5d313f036602be4d64d060565a2d9e1fa01e6
igor.engenhariadesoftware@gmail.com <igor.engenhariadesoftware@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-01-25 22:19:15 +0000

    Removendo a pasta 0.7 após ensinar como realizar o comando de geração da baseline

--- tags/0.7/trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-base/src/main/java/br/ufg/inf/es/base/validation/Validation.java
+++
@@ -1,9 +0,0 @@
-package br.ufg.inf.es.base.validation;
-
-/**
- * @author Cézar Augusto Ferreira
- */
-public interface Validation<T> {
-    
-    void validate(T object) throws ValidationException;
-}
