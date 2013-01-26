46b64eb940b2715fd895ba78a3c1058cb864e334
kssiofreitas <kssiofreitas@gmail.com>
2013-01-26 00:00:00 +0000

    ref #578 Adequações.

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/EditoraService.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/EditoraService.java
@@ -1,5 +1,7 @@
 package br.ufg.inf.es.integracao;
 
+import br.ufg.inf.es.base.validation.ValidationException;
+import br.ufg.inf.es.integracao.annotations.RNG007;
 import br.ufg.inf.es.model.Editora;
 import br.ufg.inf.es.persistencia.EditoraDAO;
 import java.util.ArrayList;
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class EditoraService extends GenericService<Editora> {
+    
+     public static final String KEY_RNG007 = "label.RNG07.nome";
 
     @Autowired
     private EditoraDAO dao;
@@ -29,17 +33,47 @@ public class EditoraService extends GenericService<Editora> {
 
         this.dao = dao;
     }
-   
-    public Collection<Editora> complete(String query) {  
+
+    public Collection<Editora> complete(String query) {
         Collection<Editora> results = new ArrayList<Editora>();
-          
-        for(Editora editora : dao.list()){
-            if(editora.getNome().contains(query)){
+
+        for (Editora editora : dao.list()) {
+            if (editora.getNome().contains(query)) {
                 results.add(editora);
             }
         }
-          
-        return results;  
-    }  
+
+        return results;
+    }
+
+    /**
+     * 
+     * @param entidade
+     * @return
+     * @throws ValidationException 
+     */
+    @Override
+    @RNG007
+    public Long insert(Editora entidade) throws ValidationException {
+
+        return this.getDAO().insert(entidade);
+
+    }
+
+    /**
+     * 
+     * @param entidade
+     * @throws ValidationException 
+     */
+    @Override
+    public void save(Editora entidade) throws ValidationException {
+      
+        Editora editora = this.getDAO().find(entidade.getId());
+      
+        editora.setNome(entidade.getNome());
+        
+        this.getDAO().save(editora);
+
+    }
     
 }
