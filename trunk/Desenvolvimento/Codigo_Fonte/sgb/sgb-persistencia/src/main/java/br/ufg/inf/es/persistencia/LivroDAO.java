6d71a6a19a74360878a846de4839afe1a255a491
kssiofreitas@gmail.com <kssiofreitas@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-03 20:44:27 +0000

    ref #725 Mudanças na página inicial da Disciplina.

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-persistencia/src/main/java/br/ufg/inf/es/persistencia/LivroDAO.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-persistencia/src/main/java/br/ufg/inf/es/persistencia/LivroDAO.java
@@ -1,11 +1,13 @@
 package br.ufg.inf.es.persistencia;
 
+import br.ufg.inf.es.base.util.UtilObjeto;
 import br.ufg.inf.es.model.Autor;
 import br.ufg.inf.es.model.Bibliografia;
 import br.ufg.inf.es.model.Livro;
 import java.util.Collection;
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -25,6 +27,7 @@ public class LivroDAO extends GenericHibernateDAO<Livro> {
     public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
+
     @Override
     protected SessionFactory getSessionFactory() {
         return this.sessionFactory;
@@ -34,13 +37,32 @@ public class LivroDAO extends GenericHibernateDAO<Livro> {
 
         return this.getCollection(id, "autores");
     }
-    
+
     public Collection<?> getBibliografia(Long id) {
+
+        //Criteria criteria = this.getSession().createCriteria(Bibliografia.class);
+
+        //criteria.add(Restrictions.eq("livro.id", id));
+
+        return this.getCollection(id, "bibliografias");
+    }
+
+    /**
+     * Método responsável por buscar um Livro de acordo com seu título.
+     *
+     * @author Cássio Augusto Silva de Freitas
+     * @param query
+     */
+    public Collection<Livro> buscaLivroPorTitulo(String query) {
+
+        Criteria criteria = this.createCriteria();
         
-       //Criteria criteria = this.getSession().createCriteria(Bibliografia.class);
-       
-       //criteria.add(Restrictions.eq("livro.id", id));
-               
-       return this.getCollection(id, "bibliografias"); 
+        if (UtilObjeto.isReferencia(query) && !query.equals("")) {
+        
+            criteria.add(Restrictions.ilike("titulo", query, MatchMode.ANYWHERE));
+        
+        }
+        
+        return criteria.list();
     }
 }
