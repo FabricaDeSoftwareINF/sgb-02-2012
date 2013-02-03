c438cca0279c1f4c3d0da24d7504b82affadf175
na.jackeline@gmail.com <na.jackeline@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-03 14:23:12 +0000

    ref #714 service

---
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/ListaComprasService.java
@@ -0,0 +1,62 @@
+package br.ufg.inf.es.integracao;
+
+import br.ufg.inf.es.base.validation.ValidationException;
+import br.ufg.inf.es.integracao.annotations.RNG000;
+import br.ufg.inf.es.model.ListaCompras;
+import br.ufg.inf.es.model.Livro;
+import br.ufg.inf.es.persistencia.ListaComprasDAO;
+import br.ufg.inf.es.persistencia.LivroDAO;
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Jackeline Neves
+ */
+@Component
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ListaComprasService extends GenericService<ListaCompras> {
+
+    @Autowired
+    private ListaComprasDAO dao;
+    
+    @Autowired
+    private LivroDAO livroDao;
+
+    public LivroDAO getLivroDao() {
+        return livroDao;
+    }
+
+    @Override
+    public ListaComprasDAO getDAO() {
+        return this.dao;
+    }
+
+    public void setDao(ListaComprasDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * 
+     * @param listaCompras 
+     */
+    public void carregarLivrosDaListaCompras(Collection<ListaCompras> listaCompras) {
+
+        for (ListaCompras lc : listaCompras) {
+            
+            Collection<Livro> livros = this.getDAO().getLivros(lc.getId());
+            
+            //for(Livro l : livros) {
+                
+              //  l.setAutores(this.getLivroDao().getAutores(l.getId()));
+                
+            //}
+            
+            lc.setLivrosDaListaCompras(livros);
+        
+        }
+    }
+}
