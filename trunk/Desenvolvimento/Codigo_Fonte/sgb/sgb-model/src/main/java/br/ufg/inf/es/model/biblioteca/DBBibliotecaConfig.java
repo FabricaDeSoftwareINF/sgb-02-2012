a268e3c3691cb28025b94a33313d736bdd7fa28a
igor.engenhariadesoftware@gmail.com <igor.engenhariadesoftware@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-08 03:23:11 +0000

    ref #743 - Correção e criação da classe de comunicação (e-mail)

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/main/java/br/ufg/inf/es/model/biblioteca/DBBibliotecaConfig.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/main/java/br/ufg/inf/es/model/biblioteca/DBBibliotecaConfig.java
@@ -1,7 +1,6 @@
 package br.ufg.inf.es.model.biblioteca;
 
 import br.ufg.inf.es.base.persistence.biblioteca.DBDriver;
-import br.ufg.inf.es.base.util.cripto.CriptoGeneric;
 import br.ufg.inf.es.model.AbstractEntityModel;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,7 +10,8 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 /**
- *
+ * Classe que representa os parâmetros de comunicação com o banco de dados da 
+ * biblioteca
  * @author igor
  */
 @Entity
@@ -42,72 +42,31 @@ public class DBBibliotecaConfig extends AbstractEntityModel{
     @Column(name="tabela", nullable=false)
     private String tabela;
     
+    @Column(name="id_livro")
+    private String campoIdLivroBiblioteca;
+    
     @Column(name="titulo_livro", nullable=false)
     private String campoTituloLivro;
+    
+    @Column(name="isbn_livro", nullable=false)
+    private String campoIsbnLivro;
+    
+    @Column(name="ano_livro")
+    private String campoAnoLivro;
+    
+    @Column(name="edicao_livro")
+    private String campoEdicao;
+    
+    @Column(name="editora_livro")
+    private String campoEditora;
+    
+    @Column(name="autor_livro")
+    private String campoAutor;
     
     @Column(name="quantidade_livro", nullable=false)
     private String campoQuantidadeLivro;
 
     public DBBibliotecaConfig() {
-    }
-
-    public DBBibliotecaConfig(DBDriver dBDriver, String status, String url, String porta, String nameDataBase, 
-            String userDataBase, byte[] passwordDataBase, String tabela, String campoTituloLivro) {
-        this.driver = dBDriver;
-        this.url = url;
-        this.porta = porta;
-        this.nameDataBase = nameDataBase;
-        this.userDataBase = userDataBase;
-        this.passwordDataBase = passwordDataBase;
-        this.tabela = tabela;
-        this.campoTituloLivro = campoTituloLivro;
-    }
-
-    public String getNameDataBase() {
-        return nameDataBase;
-    }
-
-    public void setNameDataBase(String nameDataBase) {
-        this.nameDataBase = nameDataBase;
-    }
-
-    public String getPasswordDataBase() {
-        return (passwordDataBase != null)? new String(passwordDataBase) : "";
-    }
-    
-    public byte[] getPasswordDataBaseByte(){
-        return passwordDataBase;
-    }
-
-    public void setPasswordDataBase(String passwordDataBase) {
-        byte[] passwordDataBaseCrip = new byte[TAMANHO_KEY];
-        CriptoGeneric cripto = new CriptoGeneric();
-        passwordDataBaseCrip = cripto.criptografa(passwordDataBase);
-        this.passwordDataBase = passwordDataBaseCrip;
-    }
-
-    public String getPorta() {
-        return porta;
-    }
-
-    public void setPorta(String port) {
-        this.porta = port;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUserDataBase() {
-        return userDataBase;
-    }
-
-    public void setUserDataBase(String userDataBase) {
-        this.userDataBase = userDataBase;
     }
 
     public DBDriver getDriver() {
@@ -118,12 +77,60 @@ public class DBBibliotecaConfig extends AbstractEntityModel{
         this.driver = driver;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPorta() {
+        return porta;
+    }
+
+    public void setPorta(String porta) {
+        this.porta = porta;
+    }
+
+    public String getNameDataBase() {
+        return nameDataBase;
+    }
+
+    public void setNameDataBase(String nameDataBase) {
+        this.nameDataBase = nameDataBase;
+    }
+
+    public String getUserDataBase() {
+        return userDataBase;
+    }
+
+    public void setUserDataBase(String userDataBase) {
+        this.userDataBase = userDataBase;
+    }
+
+    public byte[] getPasswordDataBase() {
+        return passwordDataBase;
+    }
+
+    public void setPasswordDataBase(byte[] passwordDataBase) {
+        this.passwordDataBase = passwordDataBase;
+    }
+
     public String getTabela() {
         return tabela;
     }
 
     public void setTabela(String tabela) {
         this.tabela = tabela;
+    }
+
+    public String getCampoIdLivroBiblioteca() {
+        return campoIdLivroBiblioteca;
+    }
+
+    public void setCampoIdLivroBiblioteca(String campoIdLivroBiblioteca) {
+        this.campoIdLivroBiblioteca = campoIdLivroBiblioteca;
     }
 
     public String getCampoTituloLivro() {
@@ -134,20 +141,64 @@ public class DBBibliotecaConfig extends AbstractEntityModel{
         this.campoTituloLivro = campoTituloLivro;
     }
 
+    public String getCampoIsbnLivro() {
+        return campoIsbnLivro;
+    }
+
+    public void setCampoIsbnLivro(String campoIsbnLivro) {
+        this.campoIsbnLivro = campoIsbnLivro;
+    }
+
+    public String getCampoAnoLivro() {
+        return campoAnoLivro;
+    }
+
+    public void setCampoAnoLivro(String campoAnoLivro) {
+        this.campoAnoLivro = campoAnoLivro;
+    }
+
+    public String getCampoEdicao() {
+        return campoEdicao;
+    }
+
+    public void setCampoEdicao(String campoEdicao) {
+        this.campoEdicao = campoEdicao;
+    }
+
+    public String getCampoEditora() {
+        return campoEditora;
+    }
+
+    public void setCampoEditora(String campoEditora) {
+        this.campoEditora = campoEditora;
+    }
+
+    public String getCampoAutor() {
+        return campoAutor;
+    }
+
+    public void setCampoAutor(String campoAutor) {
+        this.campoAutor = campoAutor;
+    }
+
     public String getCampoQuantidadeLivro() {
         return campoQuantidadeLivro;
     }
 
     public void setCampoQuantidadeLivro(String campoQuantidadeLivro) {
         this.campoQuantidadeLivro = campoQuantidadeLivro;
-    }   
+    }
 
     @Override
     public String toString() {
-        return "DBConfig{" + "dBDriver=" + driver + ", url=" + url + 
-               ", port=" + porta + ", nameDataBase=" + nameDataBase + 
-               ", userDataBase=" + userDataBase + 
-               ", passwordDataBase=" + passwordDataBase + 
-               ", tabela=" + tabela + ",s=" + campoTituloLivro +'}';
+        return "DBBibliotecaConfig{" + "driver=" + driver + ", url=" + url + 
+                ", porta=" + porta + ", nameDataBase=" + nameDataBase + 
+                ", userDataBase=" + userDataBase + ", passwordDataBase=" + 
+                passwordDataBase + ", tabela=" + tabela + ", campoIdLivroBiblioteca=" + 
+                campoIdLivroBiblioteca + ", campoTituloLivro=" + campoTituloLivro + 
+                ", campoIsbnLivro=" + campoIsbnLivro + ", campoAnoLivro=" + 
+                campoAnoLivro + ", campoEdicao=" + campoEdicao + ", campoEditora=" + 
+                campoEditora + ", campoAutor=" + campoAutor + ", "
+                + "campoQuantidadeLivro=" + campoQuantidadeLivro + '}';
     }
 }
