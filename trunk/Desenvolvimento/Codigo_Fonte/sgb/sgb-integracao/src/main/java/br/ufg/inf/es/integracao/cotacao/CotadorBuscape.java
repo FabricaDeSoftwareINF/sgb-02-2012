98659c1c205427b6decb4bbc02e8846eca93c351
inael.rodrigues@gmail.com <inael.rodrigues@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-08 00:09:29 +0000

    ref #715: implementando melhorias

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/cotacao/CotadorBuscape.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/cotacao/CotadorBuscape.java
@@ -4,27 +4,74 @@
  */
 package br.ufg.inf.es.integracao.cotacao;
 
+import br.ufg.inf.es.model.Livraria;
+import com.buscape.developer.result.type.*;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 /**
  *
  * @author Alunoinf_2
  */
-public class CotadorBuscape extends Cotador{
+public class CotadorBuscape extends Cotador {
 
-    private static final String Key = "";
-    private static final String URL = "http://sandbox.buscape.com/service/findProductList/<application_id>/?keyword=keyword";
+    private static final String Key = "564771466d477a4458664d3d";
+    private static final String URL = "http://sandbox.buscape.com/service/findProductList/564771466d477a4458664d3d/?categoryId=3482&keyword=";
+    private JAXBContext jc;
+    private Unmarshaller u;
+
+    public CotadorBuscape() {
+        try {
+            this.jc = JAXBContext.newInstance("com.buscape.developer.result.type");
+            this.u = jc.createUnmarshaller();
+        } catch (JAXBException ex) {
+            Logger.getLogger(CotadorGoogleShop.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     @Override
-    public Map<String, Map<String, String>> ConverterJsonParaDicionario(String json) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Map<Livraria, OfertaLivro> buscarOfertas(String isbn) {
+        Map<Livraria, OfertaLivro> map = new HashMap<Livraria, OfertaLivro>();
+        try {
+            Result result = (Result) u.unmarshal(new URL(gerarUrlDeBusca(isbn)));
+            for (Offer offer : result.getOffers()) {
+                PriceOffer priceOffer = offer.getPrice();
+                Seller seller = offer.getSeller();
+                String nomeLivro = offer.getOfferName();
+                String descricaoLivro = offer.getShortDescription();
+                String precoLivro = priceOffer.getValue();
+                String moeda = priceOffer.getCurrency().getAbbreviation();
+                String nomeLivraria = seller.getSellerName();
+                String linkLivraria = seller.getLinks().getLinks().get(0).getUrl();
+                String linkLivroNaLivraria = "http://buscape.com.br" + offer.getLinks().getLinks().get(0).getUrl();
+                
+                Thumbnail thumbanailLivro = offer.getThumbnail();
+                String linkImagemLIvro = thumbanailLivro==null? "": thumbanailLivro.getUrl();
+                
+                Thumbnail thumbanailLivraria = offer.getSeller().getThumbnail();
+                String linkLogoLivraria = thumbanailLivraria == null? "": thumbanailLivraria.getUrl();
+
+                OfertaLivro oferta = new OfertaLivro(nomeLivro, descricaoLivro, precoLivro, moeda, nomeLivraria, linkLivroNaLivraria, linkImagemLIvro, linkLogoLivraria);
+                Livraria livraria = new Livraria();
+                livraria.setNome(nomeLivraria);
+                livraria.setSite(linkLivraria);
+                livraria.setUrlLogo(linkLogoLivraria);
+                map.put(livraria, oferta);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(CotadorGoogleShop.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return map;
     }
 
-    @Override   
-    public String GerarUrlDeBusca(String isbn) {
-        return "http://sandbox.buscape.com/service/findOfferList/564771466d477a4458664d3d/?categoryId=3482&keyword="+isbn;
+    @Override
+    public String gerarUrlDeBusca(String isbn) {
+        return "http://sandbox.buscape.com/service/findOfferList/564771466d477a4458664d3d/?categoryId=3482&keyword=" + isbn;
     }
- 
-   
-    
 }
