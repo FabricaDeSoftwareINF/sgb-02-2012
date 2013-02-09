120172e784c7f7abb9056176574ce00c0c871bc4
igor.engenhariadesoftware@gmail.com <igor.engenhariadesoftware@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-09 15:51:52 +0000

    ref #743 - Correção Validador do Configurador de conexão com o banco de dados da Biblioteca.

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/rng/DBBibliotecaConfigValidator.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/rng/DBBibliotecaConfigValidator.java
@@ -10,76 +10,73 @@ import org.springframework.stereotype.Component;
  * @author igor
  */
 @Component
-public class DBBibliotecaConfigValidator implements Validation<DBBibliotecaConfig>{
-    
+public class DBBibliotecaConfigValidator implements Validation<DBBibliotecaConfig> {
+
     /**
-     * Método Responsável por validar os campos da entidade de configuração de 
+     * Método Responsável por validar os campos da entidade de configuração de
      * comunicação com o sistema da biblioteca.
+     *
      * @param object a ser validado
-     * @throws ValidationException 
+     * @throws ValidationException
      */
-    public void validate(DBBibliotecaConfig object) throws ValidationException {       
-        
-        if (object.getDriver() == null){
-            
+    public void validate(DBBibliotecaConfig object) throws ValidationException {
+
+        if (object.getDriver() == null) {
+
             throw new ValidationException("parametros.biblioteca.combobox.RNG018.tipoBanco");
-            
+
         } else if (object.getUrl() == null || object.getUrl().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.urlService");
-            
+
         } else if (object.getPorta() == null || object.getPorta().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.porta");
-            
+
         } else if (object.getNameDataBase() == null || object.getNameDataBase().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.nomeBancoDados");
-            
+
         } else if (object.getUserDataBase() == null || object.getUserDataBase().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.nomeUsuario");
-            
+
         } else if (object.getPasswordDataBase() == null) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.senhaUsuario");
-            
+
         } else if (object.getTabela() == null || object.getTabela().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.tabela");
-            
+
         } else if (object.getCampoTituloLivro() == null || object.getCampoTituloLivro().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.campoTituloLivro");
-            
-        } else if (object.getCampoQuantidadeLivro() == null || object.getCampoQuantidadeLivro().equals("")) {
-            
-            throw new ValidationException("parametros.biblioteca.label.RNG018.campoCampoLivro");
-            
+
         } else if (object.getCampoIdLivroBiblioteca() == null || object.getCampoIdLivroBiblioteca().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.campoISBNLivro");
-            
+
         } else if (object.getCampoAnoLivro() == null || object.getCampoAnoLivro().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.campoAnoLivro");
-            
+
         } else if (object.getCampoEdicao() == null || object.getCampoEdicao().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.campoEdicaoLivro");
-            
+
         } else if (object.getCampoEditora() == null || object.getCampoEditora().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.campoEditoraLivro");
-            
+
         } else if (object.getCampoAutor() == null || object.getCampoAutor().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.campoAutorLivro");
-            
+
         } else if (object.getCampoQuantidadeLivro() == null || object.getCampoQuantidadeLivro().equals("")) {
-            
+
             throw new ValidationException("parametros.biblioteca.label.RNG018.campoQuantidadeLivro");
-            
-        }  
+
+        }
     }
 }
