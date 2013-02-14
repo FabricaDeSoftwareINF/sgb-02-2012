935978b46fff392478c3bfb112e3897162af9c03
kssiofreitas@gmail.com <kssiofreitas@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-14 22:02:26 +0000

    ref #725 Adequações modelo ListaCompra

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/main/java/br/ufg/inf/es/model/Usuario.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/main/java/br/ufg/inf/es/model/Usuario.java
@@ -13,179 +13,203 @@ import javax.persistence.*;
 @Table(name = "USUARIO")
 public class Usuario extends AbstractEntityModel {
 
-    /** Campo senha*/
+    /**
+     * Campo senha
+     */
     private String senha;
-    
-    /** Campo email*/
+    /**
+     * Campo email
+     */
     @Column(nullable = false, unique = true)
     private String email;
-    
-    /** Campo confirmacaoEmail*/
+    /**
+     * Campo confirmacaoEmail
+     */
     @Transient
     private String confirmacaoEmail;
-    
-    /** Campo nome*/
+    /**
+     * Campo nome
+     */
     @OrderingProperty(sortOrder = SortOrder.ASC)
     private String nome;
-    
-    /** Campo sobrenome*/
+    /**
+     * Campo sobrenome
+     */
     private String sobrenome;
-    
-    /** Campo dataCadastro*/
+    /**
+     * Campo dataCadastro
+     */
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataCadastro;
-    
-    /** Campo status*/
+    /**
+     * Campo status
+     */
     private boolean status;
-    
-    /** Campo perfil*/
+    /**
+     * Campo perfil
+     */
     @Enumerated(EnumType.STRING)
     private UsuarioPerfil perfil;
 
     /**
-	 * Obtém o valor do campo <code>dataCadastros</code>
-	 *
-	 * @return {@link Date}
-	 */
+     * Obtém o valor do campo
+     * <code>dataCadastros</code>
+     *
+     * @return {@link Date}
+     */
     public Date getDataCadastro() {
 
         return (Date) dataCadastro.clone();
     }
 
     /**
-	 * Define o campo <code>dataCadastro</code>.
-	 *
-	 * @param senha 
-	 */
+     * Define o campo
+     * <code>dataCadastro</code>.
+     *
+     * @param senha
+     */
     public void setDataCadastro(Date dataCadastro) {
 
         this.dataCadastro = (Date) dataCadastro.clone();
     }
 
-	/**
-	 * Obtém o valor do campo <code>senha</code>
-	 *
-	 * @return {@link String}
-	 */
-	public String getSenha() {
-		return this.senha;
-	}
+    /**
+     * Obtém o valor do campo
+     * <code>senha</code>
+     *
+     * @return {@link String}
+     */
+    public String getSenha() {
+        return this.senha;
+    }
 
-	/**
-	 * Define o campo <code>senha</code>.
-	 *
-	 * @param senha 
-	 */
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    /**
+     * Define o campo
+     * <code>senha</code>.
+     *
+     * @param senha
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	/**
-	 * Obtém o valor do campo <code>email</code>
-	 *
-	 * @return {@link String}
-	 */
-	public String getEmail() {
-		return this.email;
-	}
+    /**
+     * Obtém o valor do campo
+     * <code>email</code>
+     *
+     * @return {@link String}
+     */
+    public String getEmail() {
+        return this.email;
+    }
 
-	/**
-	 * Define o campo <code>email</code>.
-	 *
-	 * @param email 
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    /**
+     * Define o campo
+     * <code>email</code>.
+     *
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	/**
-	 * Obtém o valor do campo <code>confirmacaoEmail</code>
-	 *
-	 * @return {@link String}
-	 */
-	public String getConfirmacaoEmail() {
-		return this.confirmacaoEmail;
-	}
+    /**
+     * Obtém o valor do campo
+     * <code>confirmacaoEmail</code>
+     *
+     * @return {@link String}
+     */
+    public String getConfirmacaoEmail() {
+        return this.confirmacaoEmail;
+    }
 
-	/**
-	 * Define o campo <code>confirmacaoEmail</code>.
-	 *
-	 * @param confirmacaoEmail 
-	 */
-	public void setConfirmacaoEmail(String confirmacaoEmail) {
-		this.confirmacaoEmail = confirmacaoEmail;
-	}
+    /**
+     * Define o campo
+     * <code>confirmacaoEmail</code>.
+     *
+     * @param confirmacaoEmail
+     */
+    public void setConfirmacaoEmail(String confirmacaoEmail) {
+        this.confirmacaoEmail = confirmacaoEmail;
+    }
 
-	/**
-	 * Obtém o valor do campo <code>nome</code>
-	 *
-	 * @return {@link String}
-	 */
-	public String getNome() {
-		return this.nome;
-	}
+    /**
+     * Obtém o valor do campo
+     * <code>nome</code>
+     *
+     * @return {@link String}
+     */
+    public String getNome() {
+        return this.nome;
+    }
 
-	/**
-	 * Define o campo <code>nome</code>.
-	 *
-	 * @param nome 
-	 */
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    /**
+     * Define o campo
+     * <code>nome</code>.
+     *
+     * @param nome
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	/**
-	 * Obtém o valor do campo <code>sobrenome</code>
-	 *
-	 * @return {@link String}
-	 */
-	public String getSobrenome() {
-		return this.sobrenome;
-	}
+    /**
+     * Obtém o valor do campo
+     * <code>sobrenome</code>
+     *
+     * @return {@link String}
+     */
+    public String getSobrenome() {
+        return this.sobrenome;
+    }
 
-	/**
-	 * Define o campo <code>sobrenome</code>.
-	 *
-	 * @param sobrenome 
-	 */
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
+    /**
+     * Define o campo
+     * <code>sobrenome</code>.
+     *
+     * @param sobrenome
+     */
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
 
-	/**
-	 * Obtém o valor do campo <code>status</code>
-	 *
-	 * @return {@link boolean}
-	 */
-	public boolean getStatus() {
-		return this.status;
-	}
+    /**
+     * Obtém o valor do campo
+     * <code>status</code>
+     *
+     * @return {@link boolean}
+     */
+    public boolean getStatus() {
+        return this.status;
+    }
 
-	/**
-	 * Define o campo <code>status</code>.
-	 *
-	 * @param status 
-	 */
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
+    /**
+     * Define o campo
+     * <code>status</code>.
+     *
+     * @param status
+     */
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
-	/**
-	 * Obtém o valor do campo <code>perfil</code>
-	 *
-	 * @return {@link UsuarioPerfil}
-	 */
-	public UsuarioPerfil getPerfil() {
-		return this.perfil;
-	}
+    /**
+     * Obtém o valor do campo
+     * <code>perfil</code>
+     *
+     * @return {@link UsuarioPerfil}
+     */
+    public UsuarioPerfil getPerfil() {
+        return this.perfil;
+    }
 
-	/**
-	 * Define o campo <code>perfil</code>.
-	 *
-	 * @param perfil 
-	 */
-	public void setPerfil(UsuarioPerfil perfil) {
-		this.perfil = perfil;
-	}
-
+    /**
+     * Define o campo
+     * <code>perfil</code>.
+     *
+     * @param perfil
+     */
+    public void setPerfil(UsuarioPerfil perfil) {
+        this.perfil = perfil;
+    }
 }
