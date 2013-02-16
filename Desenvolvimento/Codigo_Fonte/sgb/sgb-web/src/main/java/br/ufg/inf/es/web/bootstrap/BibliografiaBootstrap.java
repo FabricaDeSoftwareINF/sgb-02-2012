eea40bed8417f2ec5d683fdf9750526bc2bef216
marquete.engsoft <marquete.engsoft@gmail.com>
2013-02-16 03:58:22 +0000

    ref #781 - US15, geração da planilha de aquisição com base numa lista de cotação.

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/bootstrap/BibliografiaBootstrap.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/bootstrap/BibliografiaBootstrap.java
@@ -2,8 +2,7 @@ package br.ufg.inf.es.web.bootstrap;
 
 import br.ufg.inf.es.enuns.EnumTipoBibliografia;
 import br.ufg.inf.es.model.*;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -15,13 +14,22 @@ import org.hibernate.SessionFactory;
 public class BibliografiaBootstrap {
 
     private static final int VAGAS = 60;
-    
     private static final long ANO = 2001;
-    
+    private static final int QUANTIDADE = 5;
+    private static final double VALOR = 150;
+    private static final int QUANTIDADE2 = 6;
+    private static final double VALOR2 = 80;
+    private static Livro livro1;
+    private static Livro livro2;
+    private static Livro livro3;
+    private static Livro livro4;
     private static SessionFactory sessionFactory;
     private static Autor autor;
     private static Editora editora;
     private static Curso curso;
+    private static Livraria livraria;
+    private static ListaCotacao listaCotacao;
+    private static List<Cotacao> cotacoes;
 
     /**
      * Cria alguns dados para a verificação da lista de livros necessários para
@@ -37,15 +45,55 @@ public class BibliografiaBootstrap {
         Disciplina disciplina1 = crieDisciplina("int", "integracao");
         Disciplina disciplina2 = crieDisciplina("int2", "integracao2");
 
-        Livro livro1 = crieLivro("1402044062", "9781402044069");
-        Livro livro2 = crieLivro("1118508734", "9781118508732");
-        Livro livro3 = crieLivro("1118097599", "9781118097595");
-        Livro livro4 = crieLivro("1118130812", "9781118130810");
+        livro1 = crieLivro("1402044062", "9781402044069");
+        livro2 = crieLivro("1118508734", "9781118508732");
+        livro3 = crieLivro("1118097599", "9781118097595");
+        livro4 = crieLivro("1118130812", "9781118130810");
 
         crieBibliografia(disciplina1, livro1);
         crieBibliografia(disciplina1, livro2);
         crieBibliografia(disciplina2, livro3);
         crieBibliografia(disciplina2, livro4);
+
+        crieListaCotacao();
+    }
+
+    private static void crieListaCotacao() {
+
+        listaCotacao = new ListaCotacao();
+        listaCotacao.setNome("Lista de Cotações 1");
+        listaCotacao.setDataRealizada(Calendar.getInstance().getTime());
+        listaCotacao.setPreco(1230);
+        crieCotacoes();
+        listaCotacao.setCotacoes(cotacoes);
+        listaCotacao.setId(salve(listaCotacao));
+
+    }
+
+    private static void crieCotacoes() {
+
+        cotacoes = new ArrayList<Cotacao>();
+        crieLivraria();
+
+        Cotacao cotacao1 = new Cotacao();
+        cotacao1.setDataCadastro(Calendar.getInstance().getTime());
+        cotacao1.setLivro(livro1);
+        cotacao1.setQuantidade(QUANTIDADE);
+        cotacao1.setValor(VALOR);
+        cotacao1.setLivraria(livraria);
+        cotacao1.setId(salve(cotacao1));
+
+        Cotacao cotacao2 = new Cotacao();
+        cotacao2.setDataCadastro(Calendar.getInstance().getTime());
+        cotacao2.setLivro(livro2);
+        cotacao2.setQuantidade(QUANTIDADE2);
+        cotacao2.setValor(VALOR2);
+        cotacao2.setLivraria(livraria);
+        cotacao2.setId(salve(cotacao2));
+
+        cotacoes.add(cotacao1);
+        cotacoes.add(cotacao2);
+
     }
 
     private static void crieAutorEditoraECurso() {
@@ -62,6 +110,13 @@ public class BibliografiaBootstrap {
         curso.setNome("Engenharia de software");
         curso.setVagas(BibliografiaBootstrap.VAGAS);
         curso.setId(salve(curso));
+    }
+
+    private static void crieLivraria() {
+        livraria = new Livraria();
+        livraria.setNome("Siciliano");
+        livraria.setSite("www.siciliano.com.br");
+        livraria.setId(salve(livraria));
     }
 
     private static Bibliografia crieBibliografia(Disciplina disciplina, Livro livro) {
--- Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/ListaCotacaoController.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/ListaCotacaoController.java
@@ -3,12 +3,10 @@ package br.ufg.inf.es.web.controller;
 import br.ufg.inf.es.integracao.ListaCotacaoService;
 import br.ufg.inf.es.integracao.exportacaodados.planilha.ExportacaoPlanilhaService;
 import br.ufg.inf.es.model.Cotacao;
-import br.ufg.inf.es.model.ItemPlanilha;
 import br.ufg.inf.es.model.ListaCotacao;
+import br.ufg.inf.es.model.exportacaodados.planilha.ItemPlanilha;
 import br.ufg.inf.es.web.controller.form.ListaCotacaoForm;
 import java.io.ByteArrayInputStream;
-import java.io.FileOutputStream;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import org.primefaces.model.DefaultStreamedContent;
@@ -31,7 +29,9 @@ public class ListaCotacaoController extends SGBController<ListaCotacao, ListaCot
     @Autowired
     private ListaCotacaoService service;
     private StreamedContent arquivoXLS;
-    private StreamedContent arquivoCSV;
+    private StreamedContent arquivoCSVNacionais;
+    private StreamedContent arquivoCSVEstrangeiros;
+    ExportacaoPlanilhaService exportador = new ExportacaoPlanilhaService();
 
     @Override
     public void initData() {
@@ -49,17 +49,21 @@ public class ListaCotacaoController extends SGBController<ListaCotacao, ListaCot
         return service;
     }
 
-    public StreamedContent getArquivoCSV() {
-        return arquivoCSV;
+    public StreamedContent getArquivoCSVEstrangeiros() {
+        return arquivoCSVEstrangeiros;
+    }
+
+    public StreamedContent getArquivoCSVNacionais() {
+        return arquivoCSVNacionais;
     }
 
     public StreamedContent getArquivoXLS() {
         return arquivoXLS;
     }
 
-    public void exportarXLS(ListaCotacao listaCotacao) {
+    public void exportarXLS() {
 
-        ExportacaoPlanilhaService exportador = new ExportacaoPlanilhaService();
+        ListaCotacao listaCotacao = form.getEntity();
         List<ItemPlanilha> linhasPlanilhaNacionais = new ArrayList<ItemPlanilha>();
         List<ItemPlanilha> linhasPlanilhaEstrangeiros = new ArrayList<ItemPlanilha>();
 
@@ -73,59 +77,60 @@ public class ListaCotacaoController extends SGBController<ListaCotacao, ListaCot
                 linhasPlanilhaEstrangeiros.add(itemPlanilha);
             }
 
-            FileOutputStream out = exportador.gerarPlanilhaXLS(linhasPlanilhaNacionais, linhasPlanilhaEstrangeiros);
-            ByteArrayInputStream stream = new ByteArrayInputStream(out.toString().getBytes(Charset.forName("UTF-8")));
+            byte[] arrayBytes = exportador.gerarPlanilhaXLS(linhasPlanilhaNacionais, linhasPlanilhaEstrangeiros);
+            ByteArrayInputStream stream = new ByteArrayInputStream(arrayBytes);
 
             this.arquivoXLS = new DefaultStreamedContent(stream, "application/vnd.ms-excel", "planilha.xls");
 
         }
+
     }
 
-    public void exportarCSV(ListaCotacao listaCotacao, boolean nacionais, boolean estrangeiros) {
+    public void exportarCSVNacionais() {
 
-        ExportacaoPlanilhaService exportador = new ExportacaoPlanilhaService();
-        String strCSV = "";
+        ListaCotacao listaCotacao = form.getEntity();
+        List<ItemPlanilha> linhasPlanilhaNacionais = new ArrayList<ItemPlanilha>();
 
-        if (nacionais == true && estrangeiros == false) {
+        for (int i = 0; i < listaCotacao.getCotacoes().size(); i++) {
 
-            List<ItemPlanilha> linhasPlanilhaNacionais = new ArrayList<ItemPlanilha>();
+            if (listaCotacao.getCotacoes().get(i).getLivro().isEstrangeiro() == false) {
 
-            for (int i = 0; i < listaCotacao.getCotacoes().size(); i++) {
-
-                if (listaCotacao.getCotacoes().get(i).getLivro().isEstrangeiro() == false) {
-
-                    ItemPlanilha itemPlanilha = montarItemPlanilha(listaCotacao.getCotacoes().get(i), i + 1);
-                    linhasPlanilhaNacionais.add(itemPlanilha);
-                }
-
-                strCSV = exportador.gerarPlanilhaCSV(linhasPlanilhaNacionais);
-
+                ItemPlanilha itemPlanilha = montarItemPlanilha(listaCotacao.getCotacoes().get(i), i + 1);
+                linhasPlanilhaNacionais.add(itemPlanilha);
             }
 
-        } else if (nacionais == false && estrangeiros == true) {
-
-            List<ItemPlanilha> linhasPlanilhaEstrangeiros = new ArrayList<ItemPlanilha>();
-
-            for (int i = 0; i < listaCotacao.getCotacoes().size(); i++) {
-
-                if (listaCotacao.getCotacoes().get(i).getLivro().isEstrangeiro() == true) {
-
-                    ItemPlanilha itemPlanilha = montarItemPlanilha(listaCotacao.getCotacoes().get(i), i + 1);
-                    linhasPlanilhaEstrangeiros.add(itemPlanilha);
-                }
-
-                strCSV = exportador.gerarPlanilhaCSV(linhasPlanilhaEstrangeiros);
-
-            }
         }
 
-        ByteArrayInputStream stream = new ByteArrayInputStream(strCSV.getBytes(Charset.forName("UTF-8")));
-        arquivoCSV = new DefaultStreamedContent(stream, "text/csv", "planilha.csv");
+        byte[] arrayBytes = exportador.gerarPlanilhaCSV(linhasPlanilhaNacionais);
+        ByteArrayInputStream stream = new ByteArrayInputStream(arrayBytes);
+        arquivoCSVNacionais = new DefaultStreamedContent(stream, "text/csv", "planilha.csv");
+
+    }
+
+    public void exportarCSVEstrangeiros() {
+
+        ListaCotacao listaCotacao = form.getEntity();
+        List<ItemPlanilha> linhasPlanilhaEstrangeiros = new ArrayList<ItemPlanilha>();
+
+        for (int i = 0; i < listaCotacao.getCotacoes().size(); i++) {
+
+            if (listaCotacao.getCotacoes().get(i).getLivro().isEstrangeiro() == true) {
+
+                ItemPlanilha itemPlanilha = montarItemPlanilha(listaCotacao.getCotacoes().get(i), i + 1);
+                linhasPlanilhaEstrangeiros.add(itemPlanilha);
+            }
+
+        }
+
+        byte[] arrayBytes = exportador.gerarPlanilhaCSV(linhasPlanilhaEstrangeiros);
+        ByteArrayInputStream stream = new ByteArrayInputStream(arrayBytes);
+        arquivoCSVEstrangeiros = new DefaultStreamedContent(stream, "text/csv", "planilha.csv");
+
     }
 
     private ItemPlanilha montarItemPlanilha(Cotacao cotacao, int numItem) {
 
-        final String AREA_CONHECIMENTO = "Ciências Estas e da Terra";
+        final String AREA_CONHECIMENTO = "Ciências Exatas e da Terra";
         ItemPlanilha itemPlanilha = new ItemPlanilha();
         itemPlanilha.setNumItem(numItem);
         itemPlanilha.setTituloObra(cotacao.getLivro().getTitulo());
