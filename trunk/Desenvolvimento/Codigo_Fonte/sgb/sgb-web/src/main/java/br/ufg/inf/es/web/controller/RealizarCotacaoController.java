c6d33af99ea050a25263bb6a68cba3d2e05c093f
vinicius.esufg@gmail.com <vinicius.esufg@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-16 15:19:01 +0000

    ref #798 - Implementação US10.

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/RealizarCotacaoController.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/RealizarCotacaoController.java
@@ -4,14 +4,20 @@ import br.ufg.inf.es.integracao.RealizarCotacaoService;
 import br.ufg.inf.es.integracao.LivroService;
 import br.ufg.inf.es.model.Cotacao;
 import br.ufg.inf.es.model.ListaCotacao;
+import br.ufg.inf.es.model.Livraria;
+import br.ufg.inf.es.model.Livro;
 import br.ufg.inf.es.web.controller.form.RealizarCotacaoForm;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author vinicius
  */
+@Component
+@Scope("session")
 public class RealizarCotacaoController extends SGBController<ListaCotacao, RealizarCotacaoForm, RealizarCotacaoService> {
 
     /** Campo CotacaoForm */
@@ -31,7 +37,29 @@ public class RealizarCotacaoController extends SGBController<ListaCotacao, Reali
         
         this.getForm().setLivrosCotacao(livroService.list());
     }
-       
+
+    @Override
+    public void openInsertView() {
+     
+        Cotacao cotacao = new Cotacao();
+        
+        cotacao.setLivro(this.getService().getLivroDao().find(163840l)); 
+        
+        cotacao.setQuantidade(3);
+        
+        cotacao.setValor(79.5);
+        
+        Livraria livraria = new Livraria();
+        
+        livraria.setNome("Barnes & Noble");
+        
+        livraria.setSite("http://barnesenobles.com");
+        
+        livraria.setUrlLogo("http://1.bp.blogspot.com/-n-fsjwiesiw/Tr7UmKfH6JI/AAAAAAAAAXQ/PUbQNNaU0bY/s1600/fundacao.jpg");
+                
+        this.getForm().getCotacoesRealizadas().add(cotacao);
+        
+    }
     
     @Override
     public RealizarCotacaoForm getForm() {
