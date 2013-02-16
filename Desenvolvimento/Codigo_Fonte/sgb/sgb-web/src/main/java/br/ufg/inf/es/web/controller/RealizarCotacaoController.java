aa7069b3f34038d35b6b2ebeafb10a3123017e8d
vinicius.esufg <vinicius.esufg@gmail.com>
2013-02-16 17:11:41 +0000

    ref #798 - Implementação da tela de apresentação dos livros já cotados

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/RealizarCotacaoController.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/RealizarCotacaoController.java
@@ -2,6 +2,7 @@ package br.ufg.inf.es.web.controller;
 
 import br.ufg.inf.es.integracao.RealizarCotacaoService;
 import br.ufg.inf.es.integracao.LivroService;
+import br.ufg.inf.es.model.Autor;
 import br.ufg.inf.es.model.Cotacao;
 import br.ufg.inf.es.model.ListaCotacao;
 import br.ufg.inf.es.model.Livraria;
@@ -37,43 +38,56 @@ public class RealizarCotacaoController extends SGBController<ListaCotacao, Reali
     
     @Override
     public String openInitialPage(){
+        
         LivroDataModel livroDataModel = new LivroDataModel((List) livroService.list());
+        
         this.getForm().setLivroDataModel(livroDataModel);
+        
         return super.openInitialPage();
     }
 
     @Override
     public void openInsertView() {
      
-        Cotacao cotacao = new Cotacao();
-        
-        cotacao.setLivro(this.getService().getLivroDao().find(163840l)); 
-        
-        cotacao.setQuantidade(3);
-        
-        cotacao.setValor(79.5);
-        
-        Livraria livraria = new Livraria();
-        
-        livraria.setNome("Barnes & Noble");
-        
-        livraria.setSite("http://barnesenobles.com");
-        
-        livraria.setUrlLogo("http://1.bp.blogspot.com/-n-fsjwiesiw/Tr7UmKfH6JI/AAAAAAAAAXQ/PUbQNNaU0bY/s1600/fundacao.jpg");
-                
-        //this.getForm().getCotacoesRealizadas().add(cotacao);
+//        Cotacao cotacao = new Cotacao();
+//        
+//        Livro livro = this.getService().getLivroDao().find(163840l);
+//        
+//        livro.setAutores((Collection<Autor>) this.getService().getLivroDao().getAutores(livro.getId()));
+//        
+//        cotacao.setLivro(livro);
+//              
+//        cotacao.setQuantidade(3);
+//        
+//        cotacao.setValor(79.5);
+//        
+//        Livraria livraria = new Livraria();
+//        
+//        livraria.setNome("Barnes & Noble");
+//        
+//        livraria.setSite("http://barnesenobles.com");
+//        
+//        livraria.setUrlLogo("http://1.bp.blogspot.com/-n-fsjwiesiw/Tr7UmKfH6JI/AAAAAAAAAXQ/PUbQNNaU0bY/s1600/fundacao.jpg");
+//        
+//        List<Cotacao> cotacoes = new ArrayList<Cotacao>();
+//        
+//        cotacoes.add(cotacao);
+//        
+//        this.getForm().setCotacoesDataModel(new CotacaoDataModel(cotacoes));
         
     }
     
     @Override
     public RealizarCotacaoForm getForm() {
-        return form;
+        
+        return this.form;
     }
 
     @Override
     public RealizarCotacaoService getService() {
-        return service;
-    }
+        
+        return this.service;
+    } 
 
     public String realizarCotacao() {
         Livro[] livrosSelecionados = this.getForm().getLivrosSelecionados();
@@ -81,6 +95,7 @@ public class RealizarCotacaoController extends SGBController<ListaCotacao, Reali
         List<Cotacao> cotacoes = (List) this.service.realizarCotacao(Arrays.asList(livrosSelecionados));
         
         this.getForm().setCotacoesDataModel(new CotacaoDataModel(cotacoes));
+        
         return this.openInsertPage();
     }
     
