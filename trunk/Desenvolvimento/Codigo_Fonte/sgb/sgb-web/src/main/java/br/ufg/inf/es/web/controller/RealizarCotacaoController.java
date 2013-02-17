46bce20acfd354c19f283a7303bccc837cf3350c
vinicius.esufg@gmail.com <vinicius.esufg@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-17 17:06:55 +0000

    ref #798 adição do link da loja e da imagem do livro (ainda não carregando) na cotação.

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/RealizarCotacaoController.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/RealizarCotacaoController.java
@@ -9,9 +9,17 @@ import br.ufg.inf.es.model.Livro;
 import br.ufg.inf.es.web.controller.form.RealizarCotacaoForm;
 import br.ufg.inf.es.web.datamodel.LivroDataModel;
 import br.ufg.inf.es.web.datamodel.CotacaoDataModel;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -24,57 +32,57 @@ import org.springframework.stereotype.Component;
 @Scope("session")
 public class RealizarCotacaoController extends SGBController<ListaCotacao, RealizarCotacaoForm, RealizarCotacaoService> {
 
-    /** Campo CotacaoForm */
+    /**
+     * Campo CotacaoForm
+     */
     @Autowired
     private RealizarCotacaoForm form;
-    
     @Autowired
     private RealizarCotacaoService service;
-    
     @Autowired
     private LivroService livroService;
-    
+
     @Override
-    public String openInitialPage(){
-        
+    public String openInitialPage() {
+
         LivroDataModel livroDataModel = new LivroDataModel((List) livroService.list());
-        
+
         this.getForm().setLivroDataModel(livroDataModel);
-        
+
         return super.openInitialPage();
     }
-    
+
     @Override
     public RealizarCotacaoForm getForm() {
-        
+
         return this.form;
     }
 
     @Override
     public RealizarCotacaoService getService() {
-        
+
         return this.service;
-    } 
+    }
 
     public String realizarCotacao() {
-        
+
         Livro[] livrosSelecionados = this.getForm().getLivrosSelecionados();
-        
+
         ListaCotacao listaCotacao = this.getService().realizarCotacao(Arrays.asList(livrosSelecionados));
-        
+
         List<CotacoesLivro> cotacoes = new ArrayList<CotacoesLivro>(listaCotacao.getCotacoesLivro());
-        
+
         this.getForm().setCotacoesDataModel(new CotacaoDataModel(cotacoes));
-        
+
         this.getForm().setCotacoesSelecionadas(cotacoes);
-        
+
         return this.openInsertPage();
     }
-    
+
     public String realizarCotacaoListaCompras() {
-        
-        if(UtilObjeto.isReferencia(this.getForm().getListaCompras()) && UtilObjeto.isReferencia(this.getForm().getListaCompras().getLivrosDaListaCompras())){
-        
+
+        if (UtilObjeto.isReferencia(this.getForm().getListaCompras()) && UtilObjeto.isReferencia(this.getForm().getListaCompras().getLivrosDaListaCompras())) {
+
             ListaCotacao listaCotacao = this.getService().realizarCotacao(this.getForm().getListaCompras().getLivrosDaListaCompras());
 
             List<CotacoesLivro> cotacoes = new ArrayList<CotacoesLivro>(listaCotacao.getCotacoesLivro());
@@ -82,19 +90,30 @@ public class RealizarCotacaoController extends SGBController<ListaCotacao, Reali
             this.getForm().setCotacoesDataModel(new CotacaoDataModel(cotacoes));
 
             this.getForm().setCotacoesSelecionadas(cotacoes);
-        
+
             return this.openInsertPage();
         }
-        
+
         return "";
     }
-    
+
     public String salvarListaCotacao() {
-        
-        this.getService().salvarListaCotacao(this.getForm().getCotacoesSelecionadas(), 
+
+        this.getService().salvarListaCotacao(this.getForm().getCotacoesSelecionadas(),
                 this.getForm().getNomeLista());
-        
+
         return this.openInitialPage();
     }
-    
+
+    public StreamedContent buscaImagem() {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        String imagem = externalContext.getRequestParameterMap().get("imagem");
+        StreamedContent sc = null;
+        try {
+            sc = new DefaultStreamedContent(new URL(imagem).openStream());
+        } catch (IOException ex) {
+            Logger.getLogger(RealizarCotacaoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sc;
+    }
 }
