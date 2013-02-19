7ca55713ef987e2d78c4c3c7e7d09bfd862d1f40
marquete.engsoft <marquete.engsoft@gmail.com>
2013-02-19 13:51:44 +0000

    ref #871 - Busca de livro no template exibe em dialog os dados do livro e o valor médio atual.

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/TemplateController.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-web/src/main/java/br/ufg/inf/es/web/controller/TemplateController.java
@@ -1,8 +1,13 @@
 package br.ufg.inf.es.web.controller;
 
 import br.ufg.inf.es.integracao.LivroService;
+import br.ufg.inf.es.integracao.cotacao.CotadorBuscape;
+import br.ufg.inf.es.integracao.cotacao.CotadorGoogleShop;
+import br.ufg.inf.es.integracao.cotacao.ResultadoCotacao;
+import br.ufg.inf.es.model.CotacoesLivro;
 import br.ufg.inf.es.model.Livro;
 import br.ufg.inf.es.web.controller.form.LivroForm;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import org.primefaces.event.SelectEvent;
@@ -82,5 +87,69 @@ public class TemplateController extends SGBController<Livro, LivroForm, LivroSer
         Livro livroSelecionado = (Livro) event.getObject();
         this.getForm().setLivroSelecionado(livroSelecionado);
 
+    }
+
+    public String getValorMedio() {
+
+        double mediaCotacoes = 0;
+
+        if (this.getForm().getEntity().isEstrangeiro() == false) {
+            mediaCotacoes = getValorMedioNacional();
+        } else {
+            mediaCotacoes = getValorMedioEstrangeiro();
+        }
+
+        if (mediaCotacoes > 0) {
+            DecimalFormat decimalFormat = new DecimalFormat(); 
+            decimalFormat.setMaximumFractionDigits(2); 
+            return decimalFormat.format(mediaCotacoes);
+        } else {
+            return "Indisponível.";
+        }
+
+    }
+
+    private double getValorMedioNacional() {
+
+        CotadorBuscape cotadorBuscape = new CotadorBuscape();
+        double somaCotacoesNacionais = 0;
+        double valorMedioNacional = 0;
+
+        Livro livro = (Livro) this.getForm().getEntity();
+        ArrayList<ResultadoCotacao> cotacoesBuscape =
+                new ArrayList<ResultadoCotacao>(cotadorBuscape.buscarOfertas(livro.getIsbn13()));
+
+        if (cotacoesBuscape.size() > 0) {
+
+            for (int i = 0; i < cotacoesBuscape.size(); i++) {
+                somaCotacoesNacionais += Double.valueOf(cotacoesBuscape.get(i).getOfertaLivro().getPrecoLivro());
+            }
+            valorMedioNacional = somaCotacoesNacionais / cotacoesBuscape.size();
+
+        }
+
+        return valorMedioNacional;
+    }
+
+    private double getValorMedioEstrangeiro() {
+
+        CotadorGoogleShop cotadorGoogleShop = new CotadorGoogleShop();
+        double somaCotacoesEstrageiras = 0;
+        double valorMedioEstrangeiro = 0;
+
+        Livro livro = (Livro) this.getForm().getEntity();
+        ArrayList<ResultadoCotacao> cotacoesGoogleShop =
+                new ArrayList<ResultadoCotacao>(cotadorGoogleShop.buscarOfertas(livro.getIsbn13()));
+
+        if (cotacoesGoogleShop.size() > 0) {
+
+            for (int i = 0; i < cotacoesGoogleShop.size(); i++) {
+                somaCotacoesEstrageiras += Double.valueOf(cotacoesGoogleShop.get(i).getOfertaLivro().getPrecoLivro());
+            }
+            valorMedioEstrangeiro = somaCotacoesEstrageiras / cotacoesGoogleShop.size();
+
+        }
+
+        return valorMedioEstrangeiro;
     }
 }
