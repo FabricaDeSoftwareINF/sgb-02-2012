8a42a0f9177bc19ceba99a2f5b2b4f17824c8174
vcarvalho0402@gmail.com <vcarvalho0402@gmail.com@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-20 01:18:57 +0000

    ref #862 - adicionando testes de unidade para o servico de cotacao

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/CotacaoService.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/CotacaoService.java
@@ -1,14 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufg.inf.es.integracao;
 
 import br.ufg.inf.es.base.persistence.DAO;
-import br.ufg.inf.es.model.Cotacao;
 import br.ufg.inf.es.model.CotacoesLivro;
 import br.ufg.inf.es.model.Livro;
-import br.ufg.inf.es.persistencia.CotacaoDAO;
 import br.ufg.inf.es.persistencia.CotacoesLivroDAO;
 import br.ufg.inf.es.persistencia.LivroDAO;
 import java.util.Collection;
@@ -19,21 +13,23 @@ import org.springframework.stereotype.Component;
 
 /**
  * Classe servidora para entidade Cotacao
+ *
  * @author Marquete
  */
 @Component
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class CotacaoService extends GenericService<CotacoesLivro> {
 
-    /** Campo dao*/
+    /**
+     * Campo dao
+     */
     @Autowired
     private CotacoesLivroDAO dao;
-    
     @Autowired
     private LivroDAO livroDao;
-    
-    /** 
-     * {@inheritDoc} 
+
+    /**
+     * {@inheritDoc}
      */
     @Override
     public DAO<CotacoesLivro, Long> getDAO() {
@@ -46,22 +42,32 @@ public class CotacaoService extends GenericService<CotacoesLivro> {
      * @param dao
      */
     public void setDao(CotacoesLivroDAO dao) {
-    	
+
         this.dao = dao;
     }
 
+    /**
+     * Lista os livros
+     * @return  lista de livros
+     */
     public Collection<Livro> listarLivros() {
-        
+
         return this.getLivroDao().list();
     }
 
+    /**
+     * Obtem o dao da entidade Livro
+     * @return 
+     */
     public LivroDAO getLivroDao() {
         return livroDao;
     }
 
+    /**
+     * Define um dao para a entidade Livro
+     * @param livroDao 
+     */
     public void setLivroDao(LivroDAO livroDao) {
         this.livroDao = livroDao;
     }
-    
-    
 }
