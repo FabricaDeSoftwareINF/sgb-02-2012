5fe98a4d02699fcdb392cda9c6efd213e9e3daab
inael.rodrigues <inael.rodrigues@gmail.com>
2013-02-20 01:15:55 +0000

    ref  #792, ref #793, ref #794 , ref #795 , ref #796 : criando testes para o componente Base.

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/LivroParaCotacaoService.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-integracao/src/main/java/br/ufg/inf/es/integracao/LivroParaCotacaoService.java
@@ -1,6 +1,6 @@
 package br.ufg.inf.es.integracao;
 
-import br.ufg.inf.es.base.excptions.LivroParaCotacaoException;
+import br.ufg.inf.es.base.exceptions.LivroParaCotacaoException;
 import br.ufg.inf.es.base.persistence.DAO;
 import br.ufg.inf.es.model.dtos.LivroParaCotacao;
 import br.ufg.inf.es.base.validation.ValidationException;
