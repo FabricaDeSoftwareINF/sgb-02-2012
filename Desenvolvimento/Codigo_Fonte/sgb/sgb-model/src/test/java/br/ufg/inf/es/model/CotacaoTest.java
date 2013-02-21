8c12dedc5faddac71b52d474f7d3e7eac99b5560
inael.rodrigues <inael.rodrigues@gmail.com>
2013-02-21 02:16:04 +0000

    ref #792, ref #793, ref #794 , ref #795 , ref #796 : melhorando cobertura de testes.

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/test/java/br/ufg/inf/es/model/CotacaoTest.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/test/java/br/ufg/inf/es/model/CotacaoTest.java
@@ -2,6 +2,7 @@ package br.ufg.inf.es.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -37,14 +38,41 @@ public class CotacaoTest {
     public void testGetLivraria() {
         assertEquals(livraria, cotacao.getLivraria());
     }
-    
+
     @Test
-    public void testHashCode(){
-        assertTrue(cotacao.hashCode()!=0);
+    public void testHashCode() {
+        assertTrue(cotacao.hashCode() != 0);
     }
+
     @Test
-    public void testHashCodeLivrariaNull(){
+    public void testHashCodeLivrariaNull() {
         cotacao.setLivraria(null);
-        assertTrue(cotacao.hashCode()!=0);
+        assertTrue(cotacao.hashCode() != 0);
+    }
+
+    @Test
+    public void testEquals() {
+        assertFalse(cotacao.equals(null));
+        assertFalse(cotacao.equals(""));
+        assertFalse(cotacao.equals(new Cotacao()));
+        
+        Cotacao c = new Cotacao();
+        c.setValor(valor);
+        assertFalse(cotacao.equals(c));
+        
+        Cotacao c2 = new Cotacao();
+        Livraria a = new Livraria();
+        a.setId(1L);
+        a.setNome("a");
+        c2.setLivraria(a);
+        c2.setValor(valor);
+        assertFalse(cotacao.equals(c2));
+        
+        Cotacao c5 = new Cotacao();
+        c5.setValor(valor);
+        c5.setLivraria(livraria);
+        assertTrue(cotacao.equals(c5));
+        
+        
     }
 }
