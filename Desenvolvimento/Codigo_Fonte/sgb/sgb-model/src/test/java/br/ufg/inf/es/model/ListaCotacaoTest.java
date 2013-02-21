fd8f03ef3832c48056daab53204855d2ab477005
inael.rodrigues <inael.rodrigues@gmail.com>
2013-02-21 23:27:17 +0000

    ref #792, ref #793, ref #794 , ref #795 , ref #796 : melhorando cobertura de testes.

--- Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/test/java/br/ufg/inf/es/model/ListaCotacaoTest.java
+++ Desenvolvimento/Codigo_Fonte/sgb/sgb-model/src/test/java/br/ufg/inf/es/model/ListaCotacaoTest.java
@@ -1,5 +1,6 @@
 package br.ufg.inf.es.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -16,7 +17,8 @@ public class ListaCotacaoTest {
     private ListaCotacao listaCotacao;
     private String nome = "nome";
     private Date dataRealizada = new Date();
-    private Collection<CotacoesLivro> cotacoesLivro = Arrays.asList(new CotacoesLivro());
+     CotacoesLivro cotacao = new CotacoesLivro();
+    private Collection<CotacoesLivro> cotacoesLivro =new ArrayList<CotacoesLivro>();
 
     /**
      * setup
@@ -24,10 +26,15 @@ public class ListaCotacaoTest {
     @Before
     public void setUp() {
         listaCotacao = new ListaCotacao();
-
+       
+        cotacao.setValorMedio(5);
+        cotacao.setQuantidade(4);
+        cotacao.setLivro(new Livro());
+       
         listaCotacao.setNome(nome);
         listaCotacao.setDataRealizada(dataRealizada);
         listaCotacao.setCotacoesLivro(cotacoesLivro);
+        cotacoesLivro.add(cotacao);
     }
 
     /**
@@ -70,5 +77,60 @@ public class ListaCotacaoTest {
     @Test
     public void testGetCotacoesLivro() {
         assertEquals(cotacoesLivro, listaCotacao.getCotacoesLivro());
+    }
+
+    @Test
+    public void testGetValor() {
+        double valor = listaCotacao.getValor();
+        assertTrue(valor > 0);
+    }
+   
+    @Test
+    public void testEquals(){
+        setUp();
+        assertTrue(listaCotacao.equals(listaCotacao));
+        assertFalse(listaCotacao.equals(null));
+        assertFalse(listaCotacao.equals(" "));
+        
+        ListaCotacao listaCotacao2 = new ListaCotacao();
+        listaCotacao2.setNome("b");
+        listaCotacao2.setDataRealizada(new Date());
+        cotacoesLivro.add(new CotacoesLivro());
+        listaCotacao2.setCotacoesLivro(cotacoesLivro);
+        
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        listaCotacao2.setNome(null);
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        
+       
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        listaCotacao2.setCotacoesLivro(null);
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        listaCotacao2.setDataRealizada(null);
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        listaCotacao2.setCotacoesLivro(null);
+        
+        assertFalse(listaCotacao.equals(listaCotacao2));
+        
+    }
+     @Test
+    public void testHashCode()
+    {
+        listaCotacao.setNome(nome);
+        assertTrue(listaCotacao.hashCode()!=0);
+        listaCotacao.setNome(null);
+        assertTrue(listaCotacao.hashCode()!=0);
+        listaCotacao.setDataRealizada(new Date());
+        assertTrue(listaCotacao.hashCode()!=0);
+        listaCotacao.setDataRealizada(null);
+        assertTrue(listaCotacao.hashCode()!=0);
+        listaCotacao.setCotacoesLivro(cotacoesLivro);
+        assertTrue(listaCotacao.hashCode()!=0);
+        listaCotacao.setCotacoesLivro(null);
+        assertTrue(listaCotacao.hashCode()!=0);
     }
 }
