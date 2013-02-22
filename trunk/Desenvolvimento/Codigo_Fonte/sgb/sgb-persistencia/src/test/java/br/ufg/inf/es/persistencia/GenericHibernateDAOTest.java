f2534093c233ebe6cea2911835a29b2e8799f432
allanvribeiro1 <allanvribeiro1@cb3e217d-c7dc-add5-0c6e-bb6a71c19789>
2013-02-22 00:33:38 +0000

    ref #763: implementando cotacao nacional com o buscape

--- trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-persistencia/src/test/java/br/ufg/inf/es/persistencia/GenericHibernateDAOTest.java
+++ trunk/Desenvolvimento/Codigo_Fonte/sgb/sgb-persistencia/src/test/java/br/ufg/inf/es/persistencia/GenericHibernateDAOTest.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import org.hibernate.Criteria;
-import org.hibernate.Hibernate;
 import org.hibernate.SessionFactory;
 import org.hibernate.classic.Session;
 import org.hibernate.criterion.Criterion;
@@ -18,8 +17,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import static org.powermock.api.mockito.PowerMockito.*;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 
